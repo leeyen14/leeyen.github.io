d5cea101e2042ac908ba20397e2e5888e5a1c2e2
leeyen <libra06051992@gmail.com>
2025-03-18 09:45:20 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Cảm ơn bạn đã liên hệ! Tôi sẽ phản hồi sớm nhất có thể.');
+});
